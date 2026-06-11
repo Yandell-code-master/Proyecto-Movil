6d3b8b15ef2f19900328e9913473001a13e7fdbd
Yandell-code-master <mongeyandell@gmail.com>
2026-06-10 19:39:42 -0600

    Se hizo toda la parte de la uatorizacion

---
+++ API_Security/API_Security/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Security.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Date { get; set; }
+        public string Roll { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ API_Security/API_Security/Services/AuthorizationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace API_Security.Services
+{
+    public class AuthorizationResponse
+    {
+        public string Token { get; set; }
+        public string Msj { get; set; }
+        public bool Result { get; set; }
+    }
+}
